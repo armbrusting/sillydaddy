d8b87abaf4c05c79bbfa60e89059b098ba253b54
armbrusting <ryan.armbrust@gmail.com>
2012-05-07 21:44:41 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sillydaddy::Application.config.secret_token = 'a45eeee7a316fde5f73c244e3d4aacef544b6116826dd6587adc0dec73469f21af5fddf862e3e702c63c1d83ee452ba3c6f75079a2c88703c5ae0cac2d2a8af5'
