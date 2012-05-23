f497d1779eaa1668a11a371abcbb511760fd1d35
armbrusting <ryan.armbrust@gmail.com>
2012-05-22 22:39:42 -0400

    Version 0.2.1

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,11 @@
 class Photo < ActiveRecord::Base
   attr_accessible :content, :image, :user_id
-  has_attached_file :image, :styles => {:side => "204x204", :medium => "300x300", :thumb => "100x100" }
+  has_attached_file :image, 
+		    :styles => {:side => "204x204", 
+				:medium => "300x300", 
+				:thumb => "100x100" },
+	:s3_credentials => "::Rails.root/config/s3.yml",
+        :path => "http://sillydaddy.herokuapp.com/:attachment/:style/:id.:extension"
   
   has_many :votes
   belongs_to :user
@@ -11,3 +16,4 @@ class Photo < ActiveRecord::Base
 
   default_scope order: 'photos.created_at DESC'
 end
+
